ad183977f949938777946562b5b43741e4c84b0a
Antonio <antoniojesus.ib@gmail.com>
2019-11-22 12:34:53 +0100

    Firsts commit with the Driver class

---
+++ EjemploSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EjemploSwiftUI
+//
+//  Created by Antonio Jesús on 22/11/2019.
+//  Copyright © 2019 Antonio Jesús. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EjemploSwiftUITests/EjemploSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  EjemploSwiftUITests.swift
+//  EjemploSwiftUITests
+//
+//  Created by Antonio Jesús on 22/11/2019.
+//  Copyright © 2019 Antonio Jesús. All rights reserved.
+//
+
+import XCTest
+@testable import EjemploSwiftUI
+
+class EjemploSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
